39a57b02717cd44fee9bf23bab9bdeeeeee131c4
Matthew Wilson <Matthew.Wilson@softwire.com>
2019-11-25 11:58:19 +0000

    Part 1 Complete

---
+++ src/main/java/training/busboard/BusStopInfo.java
@@ -0,0 +1,34 @@
+package training.busboard;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BusStopInfo {
+    String lineId;
+    String destinationName;
+    int timeToStation;
+
+    public String getLineId() {
+        return lineId;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public int getTimeToStation() {
+        return timeToStation;
+    }
+
+    private String getMinsSecs() {
+        return String.format("%d mins %d secs", timeToStation / 60, timeToStation % 60);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Number: %s, Destination: %s, Time: %s", lineId, destinationName, getMinsSecs());
+    }
+}
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,37 @@
 package training.busboard;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+
+        System.out.println("Enter Bus Stop ID: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String code = "490008660N";  // scanner.nextLine();
+
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        try {
+            List<BusStopInfo> infos = client.target("https://api.tfl.gov.uk/StopPoint/" + code + "/Arrivals")
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(new GenericType<List<BusStopInfo>>() {});
+
+            infos.stream()
+                    .sorted(Comparator.comparingInt(BusStopInfo::getTimeToStation))
+                    .limit(5)
+                    .forEachOrdered(info -> System.out.println(info));
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
     }
 }	
